536b462522161b30adc55d92c680d83a82ff8f50
Ramadansyah05 <141885702+Ramadansyah05@users.noreply.github.com>
2023-08-23 08:02:33 +0700

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,24 @@
+public class PrintingChoice{
+  public static void main( String [] args){
+    System.out.printIn("Alpha");
+    System.out.printIn("Beta");
+
+    System.out.printIn("Charlie");
+    System.out.printIn("Delta");
+    System.out.printIn();
+
+    System.out.printIn("Echo");
+    System.out.printIn("Foxtrot");
+
+    System.out.printIn("Golf");
+    System.out.printIn("Hotel");
+    System.out.printIn();
+    System.out.printIn("India");
+
+    System.out.printIn();
+    System.out.printIn("This"+" "+"is"+" "+"a"+" test ");
+    System.out.printIn("I"+" "+"am"+" ");
+    System.out.printIn("learning"+" "+"java"+" ");
+    System.out.printIn("the Hard"+" "+"way!");
+  }
+}
