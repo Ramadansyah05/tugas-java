ba059dfb384f66bed6d4a148debcfb340cc278de
Ramadansyah05 <141885702+Ramadansyah05@users.noreply.github.com>
2023-08-24 08:43:57 +0700

    Add files via upload

---
+++ PrintingChoice.java
@@ -0,0 +1,25 @@
+public class PrintingChoice{
+     public static void main(String[] args) {
+        System.out.print("alpha");
+        System.out.print("Beta");
+
+        System.out.print("Charlie");
+        System.out.print("Delta");
+        System.out.println();
+
+        System.out.print("Echo");
+        System.out.print("Foxtrot");
+
+        System.out.print("Golf");
+        System.out.print("Hotel");
+        System.out.println();
+        System.out.print("India");
+
+        System.out.println();
+        System.out.print("This"+" "+"is"+" "+"a"+" Test ");
+        System.out.print("i"+" "+"am"+" ");
+        System.out.print("Learning"+" "+"Java"+" ");
+        System.out.print("teh hard"+" "+"way!");
+        
+    }
+}
