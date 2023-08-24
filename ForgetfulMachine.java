ef5c735729583e999def4238ea34239342ceedb9
Ramadansyah05 <141885702+Ramadansyah05@users.noreply.github.com>
2023-08-24 08:58:28 +0700

    Add files via upload

---
+++ ForgetfulMachine.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ForgetfulMachine {
+    public static void main(String[] args) {
+        Scanner Keyboard = new Scanner(System.In);
+
+        System.out.printin(" what city of capital france");
+        Keyboard.next();
+
+        System.out.printin(" what multiplied by");
+        keyboard.nextInt();
+
+        System.out.printin("enter number between 0.0 and 0.1");
+        keyboard.nextDouble();
+
+        System.out.printin("is there anything else you would like to say?");
+        keyboard.next();
+        
+
+    }
+    
+}
